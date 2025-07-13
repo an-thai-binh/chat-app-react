19c570bbd77eb4a3c4431e14a5b82dee88ced42e
An Binh <21130287@st.hcmuaf.edu.vn>
2025-07-13 17:10:26 +0700

    hoan thanh login

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -5,6 +5,7 @@ import z from "zod";
 import api from "../../../services/apiConfig";
 import { setAccessToken, setRefreshToken } from "../../../utils/localStorageUtils";
 import { ApiEndpoints } from "../../../constants/endpoints";
+import { useEffect, useState } from "react";
 
 const schema = z.object({
     identifier: z.string().min(1, "Username must not be blank"),
@@ -21,8 +22,12 @@ export default function LoginForm() {
 
     const navigate = useNavigate();
 
+    const [isSubmittingLogin, setIsSubmittingLogin] = useState<boolean>(false);
+    const [loginError, setLoginError] = useState<string>("");
+
     const onSubmit = async (data: FormData) => {
         try {
+            setIsSubmittingLogin(true);
             const response = await api.post(ApiEndpoints.LOGIN, data);
             if (response.data.success) {
                 const { accessToken, refreshToken } = response.data.data;
@@ -32,9 +37,16 @@ export default function LoginForm() {
             }
         } catch (error: any) {
             const message = error.response?.data.message || error.message;
+            setLoginError(message);
             console.error("Authentication Error:", message);
+        } finally {
+            setIsSubmittingLogin(false);
         }
     }
+
+    useEffect(() => {
+        document.body.style.cursor = isSubmittingLogin ? "wait" : "default";
+    }, [isSubmittingLogin]);
 
     return (
         <div className="p-3 w-[450px] bg-gray-100 rounded-md ">
@@ -44,21 +56,22 @@ export default function LoginForm() {
                 </Link>
             </div>
             <p className="text-center text-2xl font-bold">Login</p>
+            <p className="text-center text-red-500 text-sm italic">{loginError}</p>
             <div>
                 <p>Username/Email</p>
-                <input {...register("identifier")} type="text" className="w-full h-10 p-3 rounded-lg border border-gray-300 outline-gray-300" />
-                <p className="ms-1 text-red-500 text-sm">{errors.identifier?.message}</p>
+                <input {...register("identifier")} type="text" onFocus={() => setLoginError("")} className="w-full h-10 p-3 rounded-lg border border-gray-300 outline-gray-300" />
+                <p className="ms-1 text-red-500 text-sm italic">{errors.identifier?.message}</p>
             </div>
             <div className="mt-3">
                 <p>Password</p>
-                <input {...register("password")} type="password" className="w-full h-10 p-3 rounded-lg border border-gray-300 outline-gray-300" />
-                <p className="ms-1 text-red-500 text-sm">{errors.password?.message}</p>
+                <input {...register("password")} type="password" onFocus={() => setLoginError("")} className="w-full h-10 p-3 rounded-lg border border-gray-300 outline-gray-300" />
+                <p className="ms-1 text-red-500 text-sm italic">{errors.password?.message}</p>
             </div>
             <div className="flex justify-end">
                 <p className="hover:underline cursor-pointer">Forgot password</p>
             </div>
             <div className="w-fit mx-auto">
-                <button className="px-2 h-10 bg-yellow-400 font-bold rounded-md hover:bg-yellow-500" onClick={handleSubmit(onSubmit)}>Sign in</button>
+                <button className="px-2 h-10 bg-yellow-400 font-bold rounded-md hover:bg-yellow-500 disabled:bg-yellow-500" onClick={handleSubmit(onSubmit)} disabled={isSubmittingLogin}>Sign in</button>
             </div>
         </div>
     );
